5e6b9de0082ff0ac57abcb0a834c2d09342aeb05
efournier92 <efournier92@gmail.com>
2017-12-31 09:25:10 -0500

    refactor to ES6

---
+++ app_client/guesserCtrl.js
@@ -0,0 +1,57 @@
+angular
+  .module('numberGuesser')
+  .controller('guesserCtrl', guesserCtrl);
+
+function guesserCtrl($scope) {
+
+  class Range {
+    constructor(min, max) {
+      this.min = min;
+      this.max = max;
+    }
+  }
+
+  $scope.hasBegun = false;
+  $scope.testingRanges = false;
+  $scope.knownRange = new Range(0, 100);
+  $scope.startRange = $scope.knownRange;
+  $scope.maxGuesses = undefined;
+  $scope.userNum = undefined;
+  $scope.guessCount = 0;
+
+  $scope.calcMaxGuesses = function () {
+    $scope.hasBegun = true;
+    let range = $scope.knownRange.max - $scope.knownRange.min;
+    let numLog = Math.log(range);
+    let numFloor = Math.floor(numLog);
+    $scope.maxGuesses = numFloor + 1;
+  };
+
+  function findMiddleNumber(range) {
+      return Math.ceil(
+        (range.min + range.max) / 2
+      )
+  }
+
+  $scope.getNewRange = function (isWithinRange) {
+    $scope.guessCount += 1;
+    if (isWithinRange === undefined) {
+      $scope.testingRanges = true;
+      let middleNumber = findMiddleNumber($scope.knownRange);
+      $scope.guessRange = new Range($scope.knownRange.min, middleNumber);
+    } else if (isWithinRange) {
+      $scope.knownRange = $scope.guessRange;
+      let middleNumber = findMiddleNumber($scope.knownRange);
+      $scope.guessRange = new Range($scope.knownRange.min, middleNumber);
+    } else {
+      $scope.knownRange.min = $scope.guessRange.max;
+      let middleNumber = findMiddleNumber($scope.knownRange);
+      $scope.guessRange = new Range($scope.guessRange.max, middleNumber);
+    }
+    if ($scope.knownRange.max - $scope.knownRange.min <= 1) {
+      $scope.userNumber = $scope.knownRange.max;
+    }
+  }
+
+};
+
