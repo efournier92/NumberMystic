1bc03048ac0a385362532cb87eb89eb3ef63d6be
efournier92 <efournier92@gmail.com>
2018-01-01 15:01:08 -0500

    prep for Heroku deploy

--- app.js
+++ app.js
@@ -6,48 +6,32 @@ const path = require(`path`);
 const logger = require(`morgan`);
 const cookieParser = require(`cookie-parser`);
 const bodyParser = require(`body-parser`);
-// const favicon = require(`serve-favicon`);
 
 ///////////////////////////////////
 // EXPRESS
 //
 const app = express();
-
-// View Engine Setup
-app.set(`views`, path.join(__dirname, `views`));
-app.set(`view engine`, `jade`);
-
-// app.use(favicon(__dirname + `/public/logo.png`));
 app.use(logger(`dev`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// Serve Fav Icon
+const favicon = require(`serve-favicon`);
+app.use(favicon(__dirname + `/public/mystic.png`));
+
 // Static Resources
 app.use(express.static(path.join(__dirname, `public`)));
 app.use(express.static(path.join(__dirname, `app_client`)));
 
-// Else, render the index.html page for the Angular SPA
-app.use((req, res) => {
-  res.sendFile(path.join(__dirname, `app_client`, `index.html`));
-});
-
 ///////////////////////////////////
-// ERROR HANDLERS
+// ERROR HANDLING
 //
 // Forward 404 to Error Handler
 app.use((req, res, next) => {
   var err = new Error(`Not Found`);
   err.status = 404;
   next(err);
-});
-
-// Unauthorised Errors
-app.use((err, req, res, next) => {
-  if (err.name === `UnauthorizedError`) {
-    res.status(401);
-    res.json({"message" : err.name + ": " + err.message});
-  }
 });
 
 // Dev Error Handler
